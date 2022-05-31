fdf5429d4e61465612fcb0c72180769f65a8b22f
Evelyn Heike <eheike@il-k5.msk.21-school.ru>
2022-05-31 20:29:38 +0300

    fix of redirect's parse

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,11 +6,16 @@
 /*   By: eheike <eheike@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 15:27:21 by eheike            #+#    #+#             */
-/*   Updated: 2022/05/30 15:18:09 by eheike           ###   ########.fr       */
+/*   Updated: 2022/05/31 20:20:04 by eheike           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
+
+void	find_double()
+{
+
+}
 
 void	find_spec(t_tok **tokens)
 {
@@ -59,110 +64,89 @@ void	find_spec(t_tok **tokens)
 	}
 }
 
+void	new_redir(t_tok **toks, int type, int *i, int *flag)
+{
+	int		j;
+	int		a;
+	t_redir	*red;
 
+	red = create_red(type);
+	if (type % 2)
+		(*i)++;
+	else
+		(*i) += 2;
+	if ((*toks)->line[*i] == ' ')
+		(*i)++;
+	j = *i;
+	while ((*toks)->line[*i] && (*toks)->line[*i] != ' ' && (*toks)->line[*i] != '>' && (*toks)->line[*i] != '<')
+		(*i)++;
+	red->name = (char *)malloc(sizeof(char) * (*i - j + 1));
+	if (red->name == NULL)
+		return ;// вернуть ошибку
+	a = 0;
+	while (j < *i)
+		red->name[a++] = (*toks)->line[j++];
+	red->name[a] = '\0';
+	add_red(&(*toks)->redir, red);
+	(*flag)--;
+}
+
+void	rec_cmd(t_tok **toks, int *i)
+{
+	int		j;
+	int		a;
+	char	*buf;
+	char	*tmp;
+
+	if ((*toks)->line[*i] != '>' && (*toks)->line[*i] && (*toks)->line[*i] != '<')
+	{
+		j = *i;
+		a = 0;
+		while ((*toks)->line[*i] != '>' && (*toks)->line[*i] != '<')
+			(*i)++;
+		buf = (char *)malloc(sizeof(char) * (*i - j + 1));
+		if (buf == NULL)
+			return ;// вернуть ошибку
+		while (j < *i)
+			buf[a++] = (*toks)->line[j++];
+		buf[a] = '\0';
+		if ((*toks)->cmd == NULL)
+			(*toks)->cmd = buf;
+		else
+		{
+			tmp = (*toks)->cmd;
+			(*toks)->cmd = ft_strjoin((*toks)->cmd, buf);
+			free(tmp);
+		}	
+	}
+}
 
 void	parse_of_line(t_tok **tokens, char **env)
 {
 	int		i;
-	int		j;
-	int		a;
-	char	*buf[2];
+	char	*buf;
 	t_tok	*tmp;
-	t_redir	*red;
 
 	tmp = *tokens;
 	find_spec(tokens);
-	
 	while (tmp)
 	{
-		//check_env(&(tmp->line), env);
+		check_env(&(tmp->line), env);
 		i = 0;
 		while (tmp->flag_r || tmp->flag_dub_r || tmp->flag_l || tmp->flag_dub_l)
 		{
-			//red = NULL;
-			if (tmp->line[i] != '>')
-			{
-				j = i;
-				a = 0;
-				while (tmp->line[i] != '>')
-					i++;
-				buf[0] = (char *)malloc(sizeof(char) * (i - j + 1));
-				while (j < i)
-					buf[0][a++] = tmp->line[j++];
-				buf[0][a] = '\0';
-				buf[1] = ft_strtrim(buf[0], " ");
-				free(buf[0]);
-				tmp->to_ex = buf[1]; // правый редирект в приоритете, остальные игнорируются
-									 // если > - все файлы на пути кроме конечного пустеют, 
-									 // файлы рядом с остальными вариантами редиректа сохраняются такими же
-									 // каждый heredoc предлагает ввести строки до ключевой строки, 
-									 // но используется только если в череде редиректов он последний
-			}
+			rec_cmd(&tmp, &i);
 			if (tmp->line[i] == '>' && tmp->line[i + 1] == '>')
-			{
-				red = create_red(2);
-				i += 2;
-				if (tmp->line[i] == ' ')
-					i++;
-				j = i;
-				while (tmp->line[i] && tmp->line[i] != 0)
-					i++;
-				red->name = (char *)malloc(sizeof(char) * (i - j + 1));
-				a = 0;
-				while (j < i)
-					red->name[a++] = tmp->line[j++];
-				red->name[a] = '\0';
-				add_red(&tmp->redir, red);
-				tmp->flag_dub_r--;
-			}
+				new_redir(&tmp, 2, &i, &(tmp->flag_dub_r));
 			else if (tmp->line[i] == '>' && tmp->line[i + 1] != '>')
-			{
-				red = create_red(1);
-				i++;
-				if (tmp->line[i] == ' ')
-					i++;
-				j = i;
-				while (tmp->line[i] && tmp->line[i] != 0 && tmp->line[i] != '>' && tmp->line[i] != '<')
-					i++;
-				red->name = (char *)malloc(sizeof(char) * (i - j + 1));
-				a = 0;
-				while (j < i)
-					red->name[a++] = tmp->line[j++];
-				red->name[a] = '\0';
-				add_red(&tmp->redir, red);
-				tmp->flag_r--;
-			}
+				new_redir(&tmp, 1, &i, &(tmp->flag_r));
 			else if (tmp->line[i] == '<' && tmp->line[i + 1] != '<')
-			{
-				red = create_red(3);
-				i++;
-				if (tmp->line[i] == ' ')
-					i++;
-				j = i;
-				red->name = (char *)malloc(sizeof(char) * (i - j + 1));
-				a = 0;
-				while (j < i)
-					red->name[a++] = tmp->line[j++];
-				red->name[a] = '\0';
-				add_red(&tmp->redir, red);
-				tmp->flag_l--;
-			}
+				new_redir(&tmp, 3, &i, &(tmp->flag_l));
 			else if (tmp->line[i] == '<' && tmp->line[i + 1] == '<')
 			{
-				red = create_red(4);
-				i++;
-				if (tmp->line[i] == ' ')
-					i++;
-				j = i;
-				red->name = (char *)malloc(sizeof(char) * (i - j + 1));
-				a = 0;
-				while (j < i)
-					red->name[a++] = tmp->line[j++];
-				red->name[a] = '\0';
-				add_red(&tmp->redir, red);
-				tmp->flag_dub_l--;
+				new_redir(&tmp, 4, &i, &(tmp->flag_dub_l));
+				//buf = readline настроить библиотеку ридлайн и добавить функцию для считывания из ввода
 			}
-			
 		}
 		tmp = tmp->next;
 	}
@@ -247,11 +231,16 @@ int	main(int argc, char **argv, char *envp[])
 	tmp = tok;
 	while (tmp->next)
 	{
-		printf("%s, %s, %d / %d, > %d, >> %d\n", tmp->line, tmp->to_ex, tmp->num, tmp->total, tmp->flag_r, tmp->flag_dub_r);
+		printf("line : %s\ncmd : %s\n%d / %d, > %d, >> %d\n", tmp->line, tmp->cmd, tmp->num, tmp->total, tmp->flag_r, tmp->flag_dub_r);
 		//printf("%d, %s\n", tmp->redir->type_re, tmp->redir->name);
+		//printf("%p\n", tmp->redir);
 		if (tmp->redir != NULL)
-			printf("%d, %s\n", tmp->redir->next->type_re, tmp->redir->next->name);
+			//printf("%d, %s\n", tmp->redir->next->type_re, tmp->redir->next->name);
+			printf("%d, %s\n\n", tmp->redir->type_re, tmp->redir->name);
 		tmp = tmp->next;
 	}
-	printf("%s, %s, %d / %d, > %d, >> %d\n", tmp->line, tmp->to_ex, tmp->num, tmp->total, tmp->flag_r, tmp->flag_dub_r);
+	printf("line : %s\ncmd : %s\n%d / %d, > %d, >> %d\n", tmp->line, tmp->cmd, tmp->num, tmp->total, tmp->flag_r, tmp->flag_dub_r);
+	if (tmp->redir != NULL)
+		//printf("%d, %s\n", tmp->redir->next->type_re, tmp->redir->next->name);
+		printf("%d, %s\n", tmp->redir->type_re, tmp->redir->name);
 }
